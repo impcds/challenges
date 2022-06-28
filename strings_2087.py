46dc815927005f272d967a73f9ade4be191495cb
Paulo Cesar Dias de Souza <paulosouza90@gmail.com>
2022-06-28 14:10:03 -0300

    strings 2087 concluido

--- strings_2087.py
+++ strings_2087.py
@@ -1,38 +1,28 @@
 import re
 
-class Conjuntos():
-    def __init__(self, nr_frases):
-        self.frases = [input() for i in range(nr_frases)]
-        self.nr_frases = nr_frases
-
-    def conjunto_ruim(self):
-        for indice, frase in enumerate(self.frases):
-            for i in range(indice + 1, self.nr_frases):
-                print(f'log {indice} - {frase} || {self.frases[i]}')
-                if re.match(frase, self.frases[i]):
-                    return True
-
-    def bom_ou_ruim(self):
-        if self.conjunto_ruim():
-            print('Conjunto Ruim')
-        else:
-            print('Conjunto Bom')
-
 class Conjunto():
     def __init__(self, nr_frases):
-        self.frases = []
+        self.frases = {chr(i): [] for i in range(97, 123)}
         self.bom = True
         for i in range(nr_frases):
             nova = input()
-            if self.bom:
-                self.analisa(nova)
-            self.frases.append(nova)
+            self.frases[nova[0]].append(nova)
+        self.analisa()
 
-    def analisa(self, frase):
-        for i in self.frases:
-            if re.match(frase, i):
-                self.bom = False
-                return
+    def analisa(self):
+        for k, v in self.frases.items():
+            if v:
+                v.sort()
+                tamanho = len(v)
+                for indice, value in enumerate(v):
+                    for j in range(indice + 1, tamanho):
+                        if re.match(v[indice], v[j]):
+                            self.bom = False
+                            return
+    def __str__(self):
+        if self.bom:
+            return 'Conjunto Bom'
+        return 'Conjunto Ruim'
 
 
 while True:
@@ -40,15 +30,4 @@ while True:
     if c == 0:
         break
     obj = Conjunto(c)
-    if obj.bom:
-        print('Conjunto Bom')
-    else:
-        print('Conjunto Ruim')
-
-'''while True:
-    c = int(input())
-    if c == 0:
-        break
-    obj = Conjuntos(c)
-    obj.bom_ou_ruim()
-'''
+    print(obj)
